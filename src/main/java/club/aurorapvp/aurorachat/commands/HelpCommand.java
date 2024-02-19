6a16d10ea2209f1b748a8e581cfa21acb583599b
Villagers654 <thbvill@gmail.com>
2024-02-19 15:44:37 +0900

    Add '/help' command

--- src/main/java/club/aurorapvp/aurorachat/commands/HelpCommand.java
+++ src/main/java/club/aurorapvp/aurorachat/commands/HelpCommand.java
@@ -1,16 +1,21 @@
 package club.aurorapvp.aurorachat.commands;
 
+import club.aurorapvp.aurorachat.AuroraChat;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CatchUnknown;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
+import org.bukkit.entity.Player;
 
 @CommandAlias("help")
+@CommandPermission("aurorachat.command.help")
 public class HelpCommand extends BaseCommand {
 
   @CatchUnknown
   @Default
-  public void helpCommand() {
-
+  @SuppressWarnings("unused")
+  public void onHelp(Player player) {
+    player.sendMessage(AuroraChat.getInstance().getLang().getComponent("help"));
   }
 }
