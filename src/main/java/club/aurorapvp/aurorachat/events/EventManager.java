5cb7c5ee8ef5ea0d6a3803eedbea32904798fecd
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2024-08-05 19:06:14 -0400

    Don't do anything nametag related on a non-Purpur server
    Signed-off-by: Villagers654 <110007851+Villagers654@users.noreply.github.com>

--- src/main/java/club/aurorapvp/aurorachat/events/EventManager.java
+++ src/main/java/club/aurorapvp/aurorachat/events/EventManager.java
@@ -3,12 +3,20 @@ package club.aurorapvp.aurorachat.events;
 import club.aurorapvp.aurorachat.AuroraChat;
 import club.aurorapvp.aurorachat.events.listeners.NameTagEventListener;
 import club.aurorapvp.aurorachat.events.listeners.PlayerEventListener;
+import java.util.logging.Level;
 import org.bukkit.Bukkit;
 
 public class EventManager {
 
   public static void init() {
     Bukkit.getPluginManager().registerEvents(new PlayerEventListener(), AuroraChat.getInstance());
-    Bukkit.getPluginManager().registerEvents(new NameTagEventListener(), AuroraChat.getInstance());
+
+    try {
+      Class.forName("org.purpurmc.purpur.event.entity.EntityTeleportHinderedEvent");
+
+      Bukkit.getPluginManager().registerEvents(new NameTagEventListener(), AuroraChat.getInstance());
+    } catch (ClassNotFoundException e) {
+      AuroraChat.getInstance().getLogger().log(Level.INFO, "It is recommended to use Purpur!");
+    }
   }
 }
