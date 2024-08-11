d864b377aaaa3b17fcea80dafc51f2e23c9b8092
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2024-08-11 15:07:09 -0400

    Use packets for player teams due to geyser having issues
    Signed-off-by: Villagers654 <110007851+Villagers654@users.noreply.github.com>

--- src/main/java/club/aurorapvp/aurorachat/modules/TeamManager.java
+++ src/main/java/club/aurorapvp/aurorachat/modules/TeamManager.java
@@ -1,28 +1,60 @@
 package club.aurorapvp.aurorachat.modules;
 
+import club.aurorapvp.aurorachat.AuroraChat;
+import java.util.HashSet;
+import java.util.Set;
+import net.minecraft.network.protocol.game.ClientboundSetPlayerTeamPacket;
+import net.minecraft.world.scores.PlayerTeam;
+import net.minecraft.world.scores.Scoreboard;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scoreboard.Team;
 
 public class TeamManager implements Listener {
 
-  private static Team TEAM;
+  private static final PlayerTeam team = new PlayerTeam(new Scoreboard(), "AuroraChat");
+
+  private final Set<String> players = new HashSet<>();
 
   public static void init() {
-    TEAM = Bukkit.getScoreboardManager().getMainScoreboard().getTeam("AuroraChat");
-
-    if (TEAM == null) {
-      TEAM = Bukkit.getScoreboardManager().getMainScoreboard().registerNewTeam("AuroraChat");
-    }
-
-    TEAM.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
+    team.setNameTagVisibility(PlayerTeam.Visibility.NEVER);
   }
 
   @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
-  public static void onPlayerJoin(PlayerJoinEvent event) {
-    TEAM.addPlayer(event.getPlayer());
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+
+    if (!players.add(player.getName())) {
+      Bukkit.getOnlinePlayers().forEach(onlinePlayer -> sendAddPlayerToTeam(player, onlinePlayer));
+    }
+
+    CraftPlayer craftPlayer = (CraftPlayer) player;
+    ClientboundSetPlayerTeamPacket teamCreatePacket =
+        ClientboundSetPlayerTeamPacket.createAddOrModifyPacket(team, true);
+
+    craftPlayer.getHandle().connection.send(teamCreatePacket);
+
+    Bukkit.getScheduler()
+        .runTaskAsynchronously(AuroraChat.getInstance(), () -> sendTeamMembers(player));
+  }
+
+  private void sendAddPlayerToTeam(Player player, Player target) {
+    ClientboundSetPlayerTeamPacket packet =
+        ClientboundSetPlayerTeamPacket.createPlayerPacket(
+            team, player.getName(), ClientboundSetPlayerTeamPacket.Action.ADD);
+
+    ((CraftPlayer) target).getHandle().connection.send(packet);
+  }
+
+  private void sendTeamMembers(Player target) {
+    ClientboundSetPlayerTeamPacket packet =
+        ClientboundSetPlayerTeamPacket.createMultiplePlayerPacket(
+            team, players, ClientboundSetPlayerTeamPacket.Action.ADD);
+
+    ((CraftPlayer) target).getHandle().connection.send(packet);
   }
 }
